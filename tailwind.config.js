5e75f050384bcf748c3301ddb32f1fe93c5618f4
Qasim Farooq <qasimfarooq631@gmail.com>
2024-11-27 18:41:40 +0500

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Add this line to include all files in src folder with extensions like js, jsx, ts, tsx
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
